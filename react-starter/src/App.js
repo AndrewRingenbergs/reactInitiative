675d8b44f4c1fa2f9785ae37831614f976ebea30
Andrew Ringenbergs <a.ringenbergs@gmail.com>
2017-02-15 22:48:45 +1100

    Looking into react states

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -14,38 +14,98 @@ import * as firebase from 'firebase';
   firebase.initializeApp(config);
 
 class ActorChip extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+
   render() {
-    return <li>{this.props.actor.name}</li>;
+    console.log(this.props);
+    return ( 
+      <li>
+        Name: <input type="text" value={this.props.name} onChange={this.handleChange} />
+      </li>
+    );
   }
 }
 
 class ActorList extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
   render() {
-    var chips = [];
+      console.log(this.props);
+    /*var chips = [];
     this.props.actors.forEach(function(actor) {
-      chips.push(<ActorChip actor={actor} key={actor.name} />);
-    });
-    console.log(chips);
+      chips.push(<ActorChip actor={actor} key={actor.name} onChange={actor.handleChange} />);
+    }); {chips}*/
+    //console.log(chips);
     return (
       <ul className="ActorList">
-        {chips}
+        <ActorChip value={this.props.name} onChange={this.props.handleChange} />;
       </ul>
     );
   }
 }
 
-class App extends Component {
-  constructor() {
-    super();
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    /*this.state = {
       ACTORS: [
         {name: 'Actor A'},
         {name: 'Actor B'},
         {name: 'Actor C'}
-      ],
-    };
+      ]
+      };*/
+      this.state = {name: ''};
+    this.handleChange = this.handleChange.bind(this);
   }
-
+  
+ 
+  handleChange(value) {
+    this.setState({name: value});
+  }
+  /*
   componentDidMount() {
     const rootRef = firebase.database().ref();
     
@@ -63,10 +123,9 @@ class App extends Component {
         ACTORS: snap.val()
       });
     });
-  }
+  }*/
 
   render() {
-      console.log('firebase',this.state);
     return (
       <div className="App">
         <div className="App-header">
@@ -77,7 +136,10 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          <ActorList actors={this.state.ACTORS} />
+          <ActorList value={this.state.name} onChange={this.handleChange}/>
+        </div>
+        <div>
+          <EssayForm />
         </div>
       </div>
     );
